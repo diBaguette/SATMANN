06c176c0ffb3e14eba5a9b5f8fbf93c4df9210fe
diBaguette <andrs.rnso@gmail.com>
2025-11-16 14:05:25 -0500

    descripción del cambio

--- satmann-backend/server.js
+++ satmann-backend/server.js
@@ -3,12 +3,22 @@ require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const path = require('path');
 const { Pool } = require('pg');
 
 const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../psychometric_test.html'));
+});
+
+
+
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
